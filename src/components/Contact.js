f1545db645f30467d0f8a8fa06f0e40ab743ab5d
Anusha Hublikar <anusha.hublikar@ivlglobal.com>
2021-06-27 01:01:57 +0530

    bug fixes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,31 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Heading from './Reusable/Heading';
 import ContactImg from '../images/contact.jpg'
 
 const Contact = () => {
+  
+   const [buttonstate, setButtonState] = useState(true);
+
+   
 
    const phoneNumberValidation = (event) => {
     const pattern = /^(0|[1-9][0-9]*)$/;
     const inputChar = String.fromCharCode(event.charCode);
-    if (event.keyCode !== 8 && !pattern.test(inputChar)) {
+    if (!pattern.test(inputChar)) {
       event.preventDefault();
     }
    }
 
+//    const avoidBlanks = (event) => {
+//     if (/\S/.test(event.target.value)) {
+//       event.preventDefault();
+//     }
+// }
+  
+  
+  
    useEffect(() => {
-    window.onbeforeunload = () => {
+    window.onload = () => {
       for(const form of document.getElementsByTagName('form')) {
         form.reset();
       }
@@ -33,19 +45,19 @@ const Contact = () => {
            <form action="https://formspree.io/f/mnqlzqnn" method="POST">
            <div className="form-group">
            <label  className="text-white">Name</label>
-           <input type="text" className="form-control" name="name" id="name" placeholder="Your name" required/>
+           <input type="text" className="form-control" name="name" id="name" placeholder="Your name"   required autoComplete="none"/>
            </div>
            <div className="form-group">
            <label  className="text-white">Email Address</label>
-           <input type="email" className="form-control" name="email" id="email" placeholder="Your email" required/>
+           <input type="email" className="form-control" name="email" id="email" placeholder="Your email"  required autoComplete="none"/>
            </div>
            <div className="form-group">
            <label  className="text-white">Mobile Number</label>
-           <input type="text" className="form-control" name="mobile" id="mobile" maxLength="10" onKeyPress={phoneNumberValidation} placeholder="Your mobile number" required/>
+           <input type="tel" className="form-control" name="mobile" id="mobile" maxLength="10"  placeholder="Your mobile number" required autoComplete="none"/>
            </div>
            <div className="form-group">
            <label  className="text-white">Message</label>
-           <textarea type="text" className="form-control" name="mobile" id="mobile" placeholder="Your message" required/>
+           <textarea type="text" className="form-control" name="message" id="message" placeholder="Your message"   required autoComplete="none"/>
            </div>
            <button  type="submit" className="btn btn-warning btn-lg text-white">Submit</button>
            </form>
